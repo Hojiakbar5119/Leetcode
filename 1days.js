e075988b62b3ab241b3feee25a377091dda90dac
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-01 22:07:48 +0500

    nums consisting

--- 1days.js
+++ 1days.js
@@ -30,14 +30,14 @@
 
 // console.log(lengthOfLastWord("   fly me   to   the moon  "))
 
-var lengthOfLastWord = function(s) {
-    let arr = s.trim()
+// var lengthOfLastWord = function(s) {
+//     let arr = s.trim()
 
-    return arr.length - arr.lastIndexOf(' ') - 1
-};
+//     return arr.length - arr.lastIndexOf(' ') - 1
+// };
 
 
-console.log(lengthOfLastWord("   fly me   to   the moon  "))
+// console.log(lengthOfLastWord("   fly me   to   the moon  "))
 
 
 
@@ -54,4 +54,53 @@ console.log(lengthOfLastWord("   fly me   to   the moon  "))
 
 // Ustuvor masala misolida kod foydalaniladi.
 
-//? ?
+//? Valid anagram  
+//  !  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+// var isAnagram = function(s, t) {
+//     // if(s.split('').sort() !== t.split('').sort()){
+//     //     return false
+//     // } else{
+//     //     return true
+//     // }
+
+
+//     let after = s.split('').sort().join('')
+//    let last=  t.split('').sort().join('')
+
+//   if(after == last){
+//     return true
+//   }
+//   else{
+//     return false
+//   }
+
+// };
+
+
+// console.log(isAnagram('isAnagram','Ainsagram'))
+
+
+
+
+
+
+// ? Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+// 
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+var shuffle = function(nums, n) {
+    let result = []
+    for(let i=0; i<n; i++){
+
+        result.push(nums[i])
+        // result.push(num+i)
+        result.push(nums[i+n])
+    }
+    return result
+};
+
+console.log([2,5,2,3,4,7],3)
