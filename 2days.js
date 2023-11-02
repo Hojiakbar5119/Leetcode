274ff74b961a751030ad36472bb6c24b80ee2160
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-02 14:30:22 +0500

    2days

---
+++ 2days.js
@@ -0,0 +1,35 @@
+// let title = "name"
+// let obj ={
+//     name: "webbrain",
+//     title: "It Center",
+//     [title]: "Webbrain Academy",
+//     title
+// }
+// console.log(obj.name);
+// console.log(obj.title);
+// console.log(obj[title]);
+
+
+// console.log("webbrain"||true);
+// console.log("webbrain"&&true);
+// console.log("webbrain"??true);
+
+
+// console.log("==========================");
+
+// console.log(null || "webbrain");
+// console.log(null && "webbrain");
+// console.log(null ?? "webbrain");
+
+
+console.log(false || "webbrain");
+console.log(false ?? "webbrain");
+console.log(false && "webbrain");
+
+
+
+
+console.log("----------------------");
+
+
+console.log(true || "webbbrain");
