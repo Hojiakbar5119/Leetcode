45b1acb984e1fec10391393421a1835f38adbee1
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-06 21:18:46 +0500

    i'm writting error code

--- 3days.js
+++ 3days.js
@@ -1,12 +1,23 @@
-// Given an integer x, return true if x is a 
+// Given an integer x, return true if x is a
 // palindrome
 // , and false otherwise.
-console.log("react learning")
+console.log("react learning");
 
-let palindrome = ()=>{
-    console.log("this number plaindroms");
-}
+let palindrome = (integers) => {
+//   console.log("this number plaindroms");
+
+  let reverse = integers.split("").reverse();
+  // if(integer)
+  // console.log(integers.split(""));
+
+  // console.log(reverse);
+  if (integers.split("") != reverse) {
+    return false;
+  }else{
+    true
+  }
+};
 
 // console.log(palindrome());
 
-palindrome()
+palindrome("121");
