277622ca1175156cb9508fb1a50d9065ed863909
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-06 21:15:00 +0500

    palindrome

--- 3days.js
+++ 3days.js
@@ -1,1 +1,12 @@
+// Given an integer x, return true if x is a 
+// palindrome
+// , and false otherwise.
 console.log("react learning")
+
+let palindrome = ()=>{
+    console.log("this number plaindroms");
+}
+
+// console.log(palindrome());
+
+palindrome()
