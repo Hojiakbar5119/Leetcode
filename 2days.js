2ec56b0778137924644e12014de8facd14c56e13
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-06 21:11:26 +0500

    react js learning

--- 2days.js
+++ 2days.js
@@ -33,3 +33,42 @@ console.log("----------------------");
 
 
 console.log(true || "webbbrain");
+
+
+const arr = [2,5,2,3,4,7]
+const arr1 = [1,2,3,2,5,2,3,4,7]
+// console.log(arr1.concat(arr));
+
+// console.log(arr+arr1);
+// console.log(arr.join(arr1));
+
+
+// birinchi elementni olib tashlang
+
+// const numbers = [1, 2, 3, 4, 5]
+// console.log(numbers.splice(0,1))            // birinchi elementni olib tashlang
+
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+
+
+// ages.sort((a,b)=>a-b)
+
+// console.log(Math.min(...ages));
+// console.log(Math.max(...ages));
+
+// for(let i=0; i<ages.length; i++)
+
+// console.log(ages[ages.length-1], ages[0]);
+
+// console.log((ages[0]+ages[ages.length-1])*ages.length/2);
+
+
+console.log(Math.min(...ages),Math.max(...ages));
+
+
+// for(const vl of ages){
+//     // console.log(vl);
+
+// }
