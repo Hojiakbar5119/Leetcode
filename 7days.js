6d883bdc8f5a80480f845c392355dca5f00fcaaf
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-12-24 17:04:23 +0500

    codewars

---
+++ 7days.js
@@ -0,0 +1,22 @@
+// const binaryArrayToNumber = arr => {
+//     // your code
+//    const binnary =  arr.join('')
+//    return parseInt(binnary,2)
+//   };
+
+//   console.log(binaryArrayToNumber([1,0,1,0]));
+// //? done
+
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+function findAverage(array) {
+  // your code here
+  let summ = array.reduce((old, cur) => {
+    return old + cur;
+  }, 0);
+
+  return summ/array.length
+}
+
+console.log(findAverage([1, 2, 3]));
